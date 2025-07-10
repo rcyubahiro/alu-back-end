ae947b19c7cb79d46c25e4f50c98235c0d5680b2
root <root@CROBO.localdomain>
2025-07-10 15:48:57 +0200

    done

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,54 +1,24 @@
 #!/usr/bin/python3
-"""
-    python script that returns TODO list progress for a given employee ID
-    """
-    import json
-    import requests
-    from sys import argv
+"""Gather employee TODO list progress from REST API"""
 
+import requests
+import sys
 
-    if __name__ == "__main__":
-            """
-                    request user info by employee ID
-                        """
-                            request_employee = requests.get(
-                                            'https://jsonplaceholder.typicode.com/users/{}/'.format(argv[1]))
-                                """
-                                        convert json to dictionary
-                                            """
-                                                employee = json.loads(request_employee.text)
-                                                    """
-                                                            extract employee name
-                                                                """
-                                                                    employee_name = employee.get("name")
+if __name__ == "__main__":
+    employee_id = int(sys.argv[1])
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
+    todos_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(employee_id)
 
-                                                                        """
-                                                                                request user's TODO list
-                                                                                    """
-                                                                                        request_todos = requests.get(
-                                                                                                        'https://jsonplaceholder.typicode.com/users/{}/todos'.format(argv[1]))
-                                                                                            """
-                                                                                                    dictionary to store task status in boolean format
-                                                                                                        """
-                                                                                                            tasks = {}
-                                                                                                                """
-                                                                                                                        convert json to list of dictionaries
-                                                                                                                            """
-                                                                                                                                employee_todos = json.loads(request_todos.text)
-                                                                                                                                    """
-                                                                                                                                            loop through dictionary & get completed tasks
-                                                                                                                                                """
-                                                                                                                                                    for dictionary in employee_todos:
-                                                                                                                                                                tasks.update({dictionary.get("title"): dictionary.get("completed")})
+    user_response = requests.get(user_url)
+    todos_response = requests.get(todos_url)
 
-                                                                                                                                                                    """
-                                                                                                                                                                            return name, total number of tasks & completed tasks
-                                                                                                                                                                                """
-                                                                                                                                                                                    EMPLOYEE_NAME = employee_name
-                                                                                                                                                                                        TOTAL_NUMBER_OF_TASKS = len(tasks)
-                                                                                                                                                                                            NUMBER_OF_DONE_TASKS = len([k for k, v in tasks.items() if v is True])
-                                                                                                                                                                                                print("Employee {} is done with tasks({}/{}):".format(
-                                                                                                                                                                                                            EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
-                                                                                                                                                                                                    for k, v in tasks.items():
-                                                                                                                                                                                                                if v is True:
-                                                                                                                                                                                                                                print("\t {}".format(k))
+    employee_name = user_response.json().get("name")
+    todos = todos_response.json()
+
+    total_tasks = len(todos)
+    done_tasks = [task for task in todos if task.get("completed")]
+    num_done = len(done_tasks)
+
+    print("Employee {} is done with tasks({}/{}):".format(employee_name, num_done, total_tasks))
+    for task in done_tasks:
+        print("\t {}".format(task.get("title")))

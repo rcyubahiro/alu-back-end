4a9218947145d212787d5f3699386301fe6ce28b
root <root@CROBO.localdomain>
2025-07-10 15:56:56 +0200

    done

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -6,8 +6,13 @@ import sys
 
 if __name__ == "__main__":
     employee_id = int(sys.argv[1])
-    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
-    todos_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(employee_id)
+
+    user_url = (
+        "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
+    )
+    todos_url = (
+        "https://jsonplaceholder.typicode.com/todos?userId={}".format(employee_id)
+    )
 
     user_response = requests.get(user_url)
     todos_response = requests.get(todos_url)
@@ -19,6 +24,11 @@ if __name__ == "__main__":
     done_tasks = [task for task in todos if task.get("completed")]
     num_done = len(done_tasks)
 
-    print("Employee {} is done with tasks({}/{}):".format(employee_name, num_done, total_tasks))
+    print(
+        "Employee {} is done with tasks({}/{}):".format(
+            employee_name, num_done, total_tasks
+        )
+    )
     for task in done_tasks:
         print("\t {}".format(task.get("title")))
+
